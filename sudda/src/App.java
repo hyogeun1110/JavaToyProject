18d4bffa38578573e671ee1a4a9332d578ca8dbc
hyogeun <yhdok1110@naver.com>
2022-08-01 19:09:51 +0900

    commit1

---
+++ sudda/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        Player.getInstance().setPlayerNum(2);  // 플레이어 수
+        Player.getInstance().setPlayerName();  // 플레이어 이름 지정
+
+
+    }
+}
---
+++ sudda/src/Player.java
@@ -0,0 +1,37 @@
+public class Player {
+    static private int playerNum;
+    static private String[] playerName;
+
+    private static Player player;
+
+    private Player(){}
+
+    public static Player getInstance(){
+        player = new Player();
+        return player;
+    }
+
+    // 플레이하는 인원수 set
+    public void setPlayerNum(int num){
+        if (num > 5){
+            throw new RuntimeException("게임가능한 인원수는 5명 이하입니다");
+        } else
+            playerNum = num;
+    }
+    // 플레이하는 인원수 get
+    public int getPlayerNum() {
+        return playerNum;
+    }
+
+    public void setPlayerName(){
+        playerName = new String[playerNum];
+        for (int i=0; i<playerNum; i++){
+            playerName[i] = "플레이어" + (i+1) ;
+        }
+    }
+
+    public String[] getPlayerName(){
+        
+    }
+
+}
